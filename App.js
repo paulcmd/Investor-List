592249f6dec4fbbee3871cf2349fcd3cb855e3ca
paul-wachira <choxmail.pm@gmail.com>
2023-01-26 11:32:24 -0600

    ready to submit

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import { Text, View, ScrollView } from 'react-native';
+import tw from 'twrnc'
+import {data} from './data'
 
 export default function App() {
+const [dataObj] = useState(data)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <ScrollView contentContainerStyle={tw`mb-5`}>
+      <Text style={tw`text-center text-4xl font-bold`}>Investor List</Text>
+ {
+  dataObj.map((item) => (
+    <View key={item._id} 
+      style={tw`justify-between m-10 p-1 gap-2 rounded-md border-red-400 border-t-2 shadow-md pl-5 border-l-yellow-200 border-l-2 sm:flex-row`}
+      >
+      <Text style={tw`text-3xl font-bold mb-4 mt-4`}>{item.name}</Text>
+      <Text>Email: {item.email}</Text>
+      <Text>Balance: {item.balance}</Text>
+      <Text style={tw`mb-6`}>Age: {item.age}</Text>
     </View>
+  ))
+ }
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
